844ee961615ba7c673fd8697ddb5eaa745248e92
henry.juang <henry.juang@noaa.gov>
2019-11-26 16:14:40 +0000

    add dad directory for dad work.

---
+++ release/v0/dad/remap_job_MGMD_physics.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -l
+#SBATCH -o %x.o%j
+#SBATCH -J remap_task
+#SBATCH -A nems
+#SBATCH -q batch
+#SBATCH -D .
+#SBATCH -n 48
+#SBATCH -t 00:20:00
+set -ax
+. /apps/lmod/lmod/init/sh
+
+cd /scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad
+CONFIGDIR=/scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad/config
+
+##-------------------------------------------------------
+## source config file
+##-------------------------------------------------------
+
+. /scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad/config/workflow.sh repro.config-MGMD-physL150
+
+export DATA=/scratch1/NCEPDEV/stmp2/Henry.Juang/C96MGMD_physics2017011906/gfs.20170119/06/
+source /scratch1/NCEPDEV/swpc/Adam.Kubaryk/global-workflow/modulefiles/fv3gfs/fre-nctools.hera
+. /scratch1/NCEPDEV/swpc/Adam.Kubaryk/global-workflow/ush/fv3gfs_remap.sh
+
+exit 
+#!/bin/sh -l
+#SBATCH -o %x.o%j
+#SBATCH -J remap_task
+#SBATCH -A nems
+#SBATCH -q batch
+#SBATCH -D .
+#SBATCH -n 48
+#SBATCH -t 00:20:00
+set -ax
+. /apps/lmod/lmod/init/sh
+
+cd /scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad
+CONFIGDIR=/scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad/config
+
+##-------------------------------------------------------
+## source config file
+##-------------------------------------------------------
+
+. /scratch1/NCEPDEV/global/Henry.Juang/2019/NEMSfv3gfs/release/v0/dad/config/workflow.sh repro.config-MGMD-physL150
+
+export DATA=/scratch1/NCEPDEV/stmp2/Henry.Juang/C96MGMD_physics2017011906/gfs.20170119/06/
+source /scratch1/NCEPDEV/swpc/Adam.Kubaryk/global-workflow/modulefiles/fv3gfs/fre-nctools.hera
+. /scratch1/NCEPDEV/swpc/Adam.Kubaryk/global-workflow/ush/fv3gfs_remap.sh
+
+exit 
